c80a75c0a4875755dc8b8035f781406a49352be1
Ivan <tornadod@mail.ru>
2021-05-23 04:49:59 +0300

    quest 8_1 basic ready

---
+++ User_App/UserLibrary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace User_App
+{
+    public class UserLibrary
+    {
+        public string User_name { get; set; }
+        public int User_age { get; set; }
+        public string User_career { get; set; }
+        public void User_App(string user_name, int user_age, string user_career)
+        {
+            User_name = user_name;
+            User_age = user_age;
+            User_career = user_career;
+        }
+
+
+    }
+}
--- lesson8_1/Program.cs
+++ lesson8_1/Program.cs
@@ -4,14 +4,92 @@
  * приложению версию и описание.
  */
 using System;
+using System.Configuration;
+using User_App;
 
 namespace lesson8_1
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ReadAllSettings();
+            UserLibrary New_user = User_input();
+            AddUpdateAppSettings("User_name", New_user.User_name);
+            AddUpdateAppSettings("User_age", Convert.ToString(New_user.User_age));
+            AddUpdateAppSettings("User_career", New_user.User_career);
+            ReadAllSettings();
+        }
+
+        static void ReadAllSettings()
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+
+                if (appSettings.Count == 0)
+                {
+                    Console.WriteLine("AppSettings is empty.");
+                }
+                else
+                {
+                    foreach (var key in appSettings.AllKeys)
+                    {
+                        Console.WriteLine("Key: {0} Value: {1}", key, appSettings[key]);
+                    }
+                }
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error reading app settings");
+            }
+        }
+
+        static UserLibrary User_input()
+        {
+            User_App.UserLibrary New_user = new UserLibrary();
+            Console.WriteLine("Введите свое имя:");
+            New_user.User_name = Console.ReadLine();
+            Console.WriteLine("Введите свой возраст:");
+            while (New_user.User_age == 0)
+            {
+                try
+                {
+                    New_user.User_age = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Возраст нужно вводить цифрами");
+                }
+            }
+            Console.WriteLine("Введите свой род деятельности:");
+            New_user.User_career = Console.ReadLine();
+            return New_user;
+        }
+       
+
+        static void AddUpdateAppSettings(string key, string value)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if (settings[key] == null)
+                {
+                    settings.Add(key, value);
+                }
+                else
+                {
+                    settings[key].Value = value;
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error writing app settings");
+            }
         }
     }
 }
